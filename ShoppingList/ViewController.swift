4f9ab53994313a94f4bab5d07dbc61291dc5c135
jlama94 <lamajulio17@gmail.com>
2019-02-12 21:41:49 -0600

    Updates

--- ShoppingList/ViewController.swift
+++ ShoppingList/ViewController.swift
@@ -11,50 +11,40 @@ import UIKit
  A component that has a TableView must conform to the
  UITableViewDataSource protocol.
  */
-class ViewController: UIViewController, UITableViewDataSource {
-    
-    @IBOutlet weak var tableView: UITableView!
-    
+class ViewController: UIViewController{
     
     @IBOutlet var textFields: [UITextField]!
     
     
-    var shoppingCart: [String] = []
+    @IBOutlet weak var descriptionTextField: UITextField!
+   
+    @IBOutlet weak var amountTextField: UITextField!
+    
+    
+    @IBOutlet weak var itemsListArea: UITextView!
+    
+    
+    /*
+     ShoppingCart: contains the item and its price.
+    */
+    var shoppingCart: [(String, Int)] = []
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
-        self.tableView.dataSource = self
         self.dissmissKeyBoardWhenNotTouchingTextFields()
     }
     
-    // Returns the number of rows of the table view.
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return shoppingCart.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        // call the reusable row.
-        let reusableCell = self.tableView.dequeueReusableCell(withIdentifier: "MyReusableRow")!
 
-        // Dale informacion a la fila.
-        // La informacion proviene de las filas que creas mediante el objeto IndexPath.
-        // IndexPath contiene rows que son las filas que tu creas.
-        reusableCell.textLabel?.text = self.shoppingCart[indexPath.row]
-        return reusableCell
-        
-    }
     
     /*
         Dismisses the keyboard when you hit return.
-        Does not dismisses the keyboard for the number pad.
+        Does not dismisses the keyboard for number pads.
     */
     @IBAction func dismissKeyBoard(_ sender: UITextField) {
         sender.resignFirstResponder()
     }
-    
-    
     
     
     /*
@@ -68,5 +58,54 @@ class ViewController: UIViewController, UITableViewDataSource {
     func dissmissKeyBoardWhenNotTouchingTextFields() {
         self.view.addGestureRecognizer(UITapGestureRecognizer(target: self.view, action: #selector(UIView.endEditing(_:))))
     }
+    
+    /*
+     newList: clears the shopping cart and the text area
+     that displays the items from the shopping cart.
+    */
+    @IBAction func createNewList(_ sender: UIButton) {
+        self.shoppingCart.removeAll()
+        self.itemsListArea.text = ""
+    }
+    
+    /*
+        Builds a string from the array of tupples of
+        (Item, Price)
+    */
+    func buildString(_ someArray:[(String, Int)])-> String {
+        var result:String = ""
+        for tupple in  someArray {
+            result = tupple.0 + " " + String(tupple.1) + "\n"
+        }
+        return result
+    }
+
+    /*
+        Adds an item to the shopping cart when pressed.
+    */
+    @IBAction func addItemToCart(_ sender: UIButton) {
+        let descriptionText = self.descriptionTextField.text!
+        let quantityText = Int(self.amountTextField.text!)
+        
+        let product: (String, Int) = (descriptionText, quantityText!)
+    
+        // add the tupple with the responses to the array.
+        self.shoppingCart.append(product)
+        
+        // build the string with the elements of the array
+        let result = buildString(self.shoppingCart)
+        
+        // put it on the view
+        self.itemsListArea.text += result
+    }
+    
+    /*
+        Resets the description and quantity fields.
+    */
+    @IBAction func clearInputFields(_ sender: UIButton) {
+        self.descriptionTextField.text = ""
+        self.amountTextField.text = ""
+    }
+    
 }
 
